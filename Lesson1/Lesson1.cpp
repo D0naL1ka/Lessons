f9c9e0f31c2c2351658521d302efc36d3de7e298
D0naL1ka <alina.suchok00@gmail.com>
2026-05-13 20:22:31 +0300

    task

--- Lesson1/Lesson1.cpp
+++ Lesson1/Lesson1.cpp
@@ -1,32 +1,39 @@
 ﻿#include <iostream>
 #include <string>
 #include <iomanip>
-
+#include <cmath>
 //#include <locale>
 
 using namespace std;
 /*
-
 */
 int main()
 {
 	setlocale(LC_ALL, "uk_UA");
 	//setlocale(LC_ALL, "rus");
-	int age = 17;
+	int age = 7 / 3.1;
 	int score = -3;
 	int count = 0;
+	const double pi = 3.14159;
+	float x = 2.5f;  // суфікс f для float
+
+	cout << age << endl;  // 135
 
 	// Операції:
-	cout << age + 10 << endl;  // 27
+	cout << "Operation";
+	cout << age + 10*5 << endl;  // 135
 	cout << age * 2 << endl;  // 34
 	cout << 17 / 5 << endl;  // 3
-	cout << 17 % 5 << endl;  // 2
+	cout << 17 % 2 << endl;  // 1
+
+
 
 	double price = 99.90;
-	double pi = 3.14159;
-
+	
+	/*
 	// Форматований вивід:
 	cout << fixed << setprecision(2)<< price << endl; // 99.90
+	cout << fixed << setprecision(2)<< pi << endl; // 3.14
 
 	string name = "Олена";
 	string city = "Київ";
@@ -51,9 +58,6 @@ int main()
 	// to_string
 	string b = to_string(100);
 
-
-
-
 	// bool — булевий тип
 	bool is_student = true;
 	bool has_done_hw = false;
@@ -63,8 +67,6 @@ int main()
 
 	// Виведення словом:
 	cout << boolalpha << is_student<<"\n";  // true
-
-
 
 	//Константи — const / readonly
 	const double PI = 3.14159;
@@ -79,7 +81,7 @@ int main()
 	setw(n) (Set Width) - ширина наступного поля
 	setprecision(n) - кількість знаків після коми
 	setfill('c') — заповнює порожні місця
-	*/
+
 
 	cout << "Пі: " << fixed << setprecision(2) << PI << "\n\n";
 
@@ -90,18 +92,7 @@ int main()
 	cout << left << setw(15) << "Ананас" << "|" << setw(10) << "120.00" << "\n";
 	cout << endl;
 
-	//Логічні оператори 
-	int age_1 = 15;
-
-	// && — обидві умови true
-	cout << (age_1 > 10 && age_1 < 20) << endl;  // 1
-
-	// || — хоча б одна true
-	cout << (age_1 < 10 || age_1>15)<<endl;  // 1
-
-	// ! — інвертує
-	cout << !true << endl;               // 0
-
+              // 0
 
 	int a, c;
 	cout << "Перше: ";  cin >> a;
@@ -112,4 +103,7 @@ int main()
 	cout << a << "> " << c << " -> " << (a > c) << "\n";
 	cout << a << "< " << c << " -> " << (a < c) << "\n";
 
+	*/
+
+return 0;
 }
